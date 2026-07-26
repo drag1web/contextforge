aa3a2041176f600a2e8b0a40061666b3cd600580
drag1web <agudovshchikov@gmail.com>
2026-07-26 14:11:45 +0500

    chore(ui): remove unused AGENTS preview imports

--- apps/desktop/renderer/src/components/modals/AgentsPreviewModal.tsx
+++ apps/desktop/renderer/src/components/modals/AgentsPreviewModal.tsx
@@ -4,9 +4,7 @@ import { useTranslation } from "react-i18next";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import {
-  Bot,
   Check,
-  Clock3,
   Code2,
   Copy,
   Edit3,
